3a9b31e9bbc8becd18b7cd801dd662c4dd0a7854
Ruan Rodrigues <rupriwell123@gmail.com>
2022-03-22 18:54:07 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -80,6 +80,6 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
-		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -37,7 +37,7 @@ public class Order implements Serializable {
 	private User client;
 	
 	@OneToMany(mappedBy = "id.order")
-	private Set<OrderItem> items =new HashSet<>();
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 	}
